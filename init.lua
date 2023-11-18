3b677bde34bfbd805e0b473cfee7fc585ddce7f9
hmelica <hippolytemelica@icloud.com>
2023-11-18 10:10:49 +0100

    update

--- init.lua
+++ init.lua
@@ -24,6 +24,8 @@ if version.cmp(ev, actual_ver) ~= 0 then
   vim.api.nvim_err_writeln(msg)
 end
 
+vim.env.PATH = '/mnt/nfs/homes/hmelica/.config/nvim/venv_nvim/bin:/mnt/nfs/homes/hmelica/tools/node-v14.15.4-linux-x64/bin:' .. vim.env.PATH
+
 local core_conf_files = {
   "globals.lua", -- some global settings
   "options.vim", -- setting options in nvim
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -486,7 +486,23 @@ local plugin_specs = {
       require("config.fidget-nvim")
     end,
   },
-  {"wakatime/vim-wakatime"}
+  {"wakatime/vim-wakatime"},
+  {
+    "linux-cultist/venv-selector.nvim",
+    dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
+    opts = {
+        -- Your options go here
+        -- name = "venv",
+        -- auto_refresh = false
+    },
+    event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+    keys = {
+        -- Keymap to open VenvSelector to pick a venv.
+        { "<leader>vs", "<cmd>VenvSelect<cr>" },
+        -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
+        { "<leader>vc", "<cmd>VenvSelectCached<cr>" },
+    }
+  }
 }
 
 -- configuration for lazy itself.
