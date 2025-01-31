c92a3007a9c09b2f26d212eb7fe1ff52e0294cb6
Hippolyte Melica <hippolytemelica@gmail.com>
2025-01-31 11:45:02 +0100

    format

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -8,109 +8,109 @@ local utils = require("utils")
 
 -- set quickfix list from diagnostics in a certain buffer, not the whole workspace
 local set_qflist = function(buf_num, severity)
-    local diagnostics = nil
-    diagnostics = diagnostic.get(buf_num, { severity = severity })
+  local diagnostics = nil
+  diagnostics = diagnostic.get(buf_num, { severity = severity })
 
-    local qf_items = diagnostic.toqflist(diagnostics)
-    vim.fn.setqflist({}, " ", { title = "Diagnostics", items = qf_items })
+  local qf_items = diagnostic.toqflist(diagnostics)
+  vim.fn.setqflist({}, " ", { title = "Diagnostics", items = qf_items })
 
-    -- open quickfix by default
-    vim.cmd([[copen]])
+  -- open quickfix by default
+  vim.cmd([[copen]])
 end
 
 local custom_attach = function(client, bufnr)
-    -- Mappings.
-    local map = function(mode, l, r, opts)
-        opts = opts or {}
-        opts.silent = true
-        opts.buffer = bufnr
-        keymap.set(mode, l, r, opts)
-    end
+  -- Mappings.
+  local map = function(mode, l, r, opts)
+    opts = opts or {}
+    opts.silent = true
+    opts.buffer = bufnr
+    keymap.set(mode, l, r, opts)
+  end
 
-    map("n", "gd", vim.lsp.buf.definition, { desc = "go to definition" })
-    map("n", "<C-]>", vim.lsp.buf.definition)
-    map("n", "K", vim.lsp.buf.hover)
-    map("n", "<C-k>", vim.lsp.buf.signature_help)
-    map("n", "<space>rn", vim.lsp.buf.rename, { desc = "varialbe rename" })
-    map("n", "gr", vim.lsp.buf.references, { desc = "show references" })
-    map("n", "[d", diagnostic.goto_prev, { desc = "previous diagnostic" })
-    map("n", "]d", diagnostic.goto_next, { desc = "next diagnostic" })
-    -- this puts diagnostics from opened files to quickfix
-    map("n", "<space>qw", diagnostic.setqflist, { desc = "put window diagnostics to qf" })
-    -- this puts diagnostics from current buffer to quickfix
-    map("n", "<space>qb", function()
-        set_qflist(bufnr)
-    end, { desc = "put buffer diagnostics to qf" })
-    map("n", "<space>ca", vim.lsp.buf.code_action, { desc = "LSP code action" })
-    map("n", "<space>wa", vim.lsp.buf.add_workspace_folder, { desc = "add workspace folder" })
-    map("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, { desc = "remove workspace folder" })
-    map("n", "<space>wl", function()
-        inspect(vim.lsp.buf.list_workspace_folders())
-    end, { desc = "list workspace folder" })
+  map("n", "gd", vim.lsp.buf.definition, { desc = "go to definition" })
+  map("n", "<C-]>", vim.lsp.buf.definition)
+  map("n", "K", vim.lsp.buf.hover)
+  map("n", "<C-k>", vim.lsp.buf.signature_help)
+  map("n", "<space>rn", vim.lsp.buf.rename, { desc = "varialbe rename" })
+  map("n", "gr", vim.lsp.buf.references, { desc = "show references" })
+  map("n", "[d", diagnostic.goto_prev, { desc = "previous diagnostic" })
+  map("n", "]d", diagnostic.goto_next, { desc = "next diagnostic" })
+  -- this puts diagnostics from opened files to quickfix
+  map("n", "<space>qw", diagnostic.setqflist, { desc = "put window diagnostics to qf" })
+  -- this puts diagnostics from current buffer to quickfix
+  map("n", "<space>qb", function()
+    set_qflist(bufnr)
+  end, { desc = "put buffer diagnostics to qf" })
+  map("n", "<space>ca", vim.lsp.buf.code_action, { desc = "LSP code action" })
+  map("n", "<space>wa", vim.lsp.buf.add_workspace_folder, { desc = "add workspace folder" })
+  map("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, { desc = "remove workspace folder" })
+  map("n", "<space>wl", function()
+    inspect(vim.lsp.buf.list_workspace_folders())
+  end, { desc = "list workspace folder" })
 
-    -- Set some key bindings conditional on server capabilities
-    if client.server_capabilities.documentFormattingProvider then
-        map("n", "<space>f", vim.lsp.buf.format, { desc = "format code" })
-    end
+  -- Set some key bindings conditional on server capabilities
+  if client.server_capabilities.documentFormattingProvider then
+    map("n", "<space>f", vim.lsp.buf.format, { desc = "format code" })
+  end
 
-    api.nvim_create_autocmd("CursorHold", {
-        buffer = bufnr,
-        callback = function()
-            local float_opts = {
-                focusable = false,
-                -- close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
-                close_events = { "BufLeave", "CursorMoved", "InsertEnter" },
-                border = "rounded",
-                source = "always", -- show source in diagnostic popup window
-                prefix = " ",
-            }
+  api.nvim_create_autocmd("CursorHold", {
+    buffer = bufnr,
+    callback = function()
+      local float_opts = {
+        focusable = false,
+        -- close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+        close_events = { "BufLeave", "CursorMoved", "InsertEnter" },
+        border = "rounded",
+        source = "always", -- show source in diagnostic popup window
+        prefix = " ",
+      }
 
-            if not vim.b.diagnostics_pos then
-                vim.b.diagnostics_pos = { nil, nil }
-            end
+      if not vim.b.diagnostics_pos then
+        vim.b.diagnostics_pos = { nil, nil }
+      end
 
-            local cursor_pos = api.nvim_win_get_cursor(0)
-            if
-                (cursor_pos[1] ~= vim.b.diagnostics_pos[1] or cursor_pos[2] ~= vim.b.diagnostics_pos[2])
-                and #diagnostic.get() > 0
-            then
-                diagnostic.open_float(nil, float_opts)
-            end
+      local cursor_pos = api.nvim_win_get_cursor(0)
+      if
+        (cursor_pos[1] ~= vim.b.diagnostics_pos[1] or cursor_pos[2] ~= vim.b.diagnostics_pos[2])
+        and #diagnostic.get() > 0
+      then
+        diagnostic.open_float(nil, float_opts)
+      end
 
-            vim.b.diagnostics_pos = cursor_pos
-        end,
-    })
+      vim.b.diagnostics_pos = cursor_pos
+    end,
+  })
 
-    -- The blow command will highlight the current variable and its usages in the buffer.
-    if client.server_capabilities.documentHighlightProvider then
-        vim.cmd([[
+  -- The blow command will highlight the current variable and its usages in the buffer.
+  if client.server_capabilities.documentHighlightProvider then
+    vim.cmd([[
       hi! link LspReferenceRead Visual
       hi! link LspReferenceText Visual
       hi! link LspReferenceWrite Visual
     ]])
 
-        local gid = api.nvim_create_augroup("lsp_document_highlight", { clear = true })
-        api.nvim_create_autocmd("CursorHold", {
-            group = gid,
-            buffer = bufnr,
-            callback = function()
-                lsp.buf.document_highlight()
-            end,
-        })
+    local gid = api.nvim_create_augroup("lsp_document_highlight", { clear = true })
+    api.nvim_create_autocmd("CursorHold", {
+      group = gid,
+      buffer = bufnr,
+      callback = function()
+        lsp.buf.document_highlight()
+      end,
+    })
 
-        api.nvim_create_autocmd("CursorMoved", {
-            group = gid,
-            buffer = bufnr,
-            callback = function()
-                lsp.buf.clear_references()
-            end,
-        })
-    end
+    api.nvim_create_autocmd("CursorMoved", {
+      group = gid,
+      buffer = bufnr,
+      callback = function()
+        lsp.buf.clear_references()
+      end,
+    })
+  end
 
-    if vim.g.logging_level == "debug" then
-        local msg = string.format("Language server %s started!", client.name)
-        vim.notify(msg, vim.log.levels.DEBUG, { title = "Nvim-config" })
-    end
+  if vim.g.logging_level == "debug" then
+    local msg = string.format("Language server %s started!", client.name)
+    vim.notify(msg, vim.log.levels.DEBUG, { title = "Nvim-config" })
+  end
 end
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
@@ -319,10 +319,10 @@ fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHin
 
 -- global config for diagnostic
 diagnostic.config {
-    underline = false,
-    virtual_text = false,
-    signs = true,
-    severity_sort = true,
+  underline = false,
+  virtual_text = false,
+  signs = true,
+  severity_sort = true,
 }
 
 -- lsp.handlers["textDocument/publishDiagnostics"] = lsp.with(lsp.diagnostic.on_publish_diagnostics, {
@@ -334,5 +334,5 @@ diagnostic.config {
 
 -- Change border of documentation hover window, See https://github.com/neovim/neovim/pull/13998.
 lsp.handlers["textDocument/hover"] = lsp.with(vim.lsp.handlers.hover, {
-    border = "rounded",
+  border = "rounded",
 })
