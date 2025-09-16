6bee150e36e35b1ab2488afdb6221d97fcc9a767
Hippolyte Melica <hippolytemelica@gmail.com>
2025-09-16 21:54:11 +0200

    regexp changing branch, updating config

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -573,7 +573,6 @@ local plugin_specs = {
       -- auto_refresh = false
     },
     event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
-    branch = "regexp", -- This is the regexp branch, use this for the new version
     keys = {
       -- Keymap to open VenvSelector to pick a venv.
       { "<leader>vs", "<cmd>VenvSelect<cr>" },
